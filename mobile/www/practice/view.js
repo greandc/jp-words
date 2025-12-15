6a7b36cb59e9872c2fe12aa427a738fda8a726d6
greandc <u1.chikami.7777@gmail.com>
2025-12-15 20:43:36 +0900

    Mac2

--- mobile/www/practice/view.js
+++ mobile/www/practice/view.js
@@ -41,12 +41,11 @@ function cleanup() {
   shell.style.overflow = "hidden";
 
   const div = document.createElement("div");
-  div.className = "screen";
+  div.className = "screen practice-screen";
   // ★ 追加：縦方向中央寄せ & 左右はそのまま
   div.style.display = "flex";
   div.style.flexDirection = "column";
-  div.style.paddingTop = "200px";         // ←追加：画面高さの10%分だけ上に寄せる
-  div.style.paddingBottom = "-30vh";       // ←追加：バランス確保（任意）
+ 
   
   div.innerHTML = `
     <h1>${t("level.label",{n:levelNum})}</h1>
